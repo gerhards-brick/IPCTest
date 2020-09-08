af83192ecf59ef81acc878f094adb467ba89c8cf
Luca Gerhards <luca.gerhards@brickmakers.de>
2020-09-08 14:03:07 +0200

    Added Creation of servicehost
    Taken from 3f8efeca2ccb658ec6d5115022587ebcf2ff4000

--- IPCTest/IPCHost.NETFramework/Program.cs
+++ IPCTest/IPCHost.NETFramework/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace IPCHost.NETFramework
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using IPCHost.NETFramework.WCF;
+
+namespace IPCHost.NETFramework
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        public void CreateHost()
+        {
+            var endpoint = new Uri("net.pip://localhost/Testpipe");
+            var serviceHost = new ServiceHost(typeof(TestService));
+            var binding = new NetNamedPipeBinding();
+
+            serviceHost.AddServiceEndpoint(typeof(ITestService), binding, endpoint);
+            serviceHost.Open();
         }
     }
 }
